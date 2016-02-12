2f0c8e7aa6f0520bfdbeb3d712400ed896103852
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 21:00:43 -0800

    successfully logs in users

--- app/models/userController.js
+++ app/models/userController.js
@@ -1,19 +1,28 @@
-  var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt-nodejs');
+var User = require('./user.js');
+var Promise = require('bluebird');
 
- 
+ var mongoose = require('mongoose');
   exports.initialize = function() {
     this.on('creating', this.hashPassword);
   };
 
   exports.comparePassword = function(attemptedPassword, user, callback) {
     // Refactor mongoDB
-    User.FindOne({username: user}, function (err, user) {
+    User.findOne({username: user}, function (err, user) {
       if (err) {
         console.log("err ",err);
       } else {
-        bcrypt.compare(attemptedPassword, user.password, function(err, isMatch) {
-          callback(isMatch);
-        });
+        console.log("compar pass ", attemptedPassword, user.password);
+        if (attemptedPassword === user.password) {
+          callback(true);
+        } else {
+          callback(false);
+        }
+        // bcrypt.compare(attemptedPassword, user.password, function(err, isMatch) {
+        //   console.log("match ",isMatch);
+        //   callback(isMatch);
+        // });
       }
     });
   };
